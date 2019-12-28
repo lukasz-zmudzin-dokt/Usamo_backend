5011320b26a353ed1923c691a40c80a962a4a38c
jan-polaczek <jan.polaczek@interia.pl>
2019-12-28 12:32:05 +0100

    added test classes and setUp methods; added cv_test_data which contains sample cv data; added cv_pic.png as a sample picture for testing

--- src/cv/tests.py
+++ src/cv/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from cv.models import CV
+from cv.cv_test_data import cv_test_data
+
+
+class GenerateTestCase(APITestCase):
+    @classmethod
+    def setUp(cls):
+        cls.url = 'cv/generate/'
+        cls.token = 'insert_token_here'
+        cls.cv_data = cv_test_data
+
+
+class DataTestCase(APITestCase):
+    @classmethod
+    def setUp(cls):
+        cls.url = 'cv/data/'
+        cls.token = 'insert_token_here'
+        cls.cv_data = cv_test_data
+
+
+class PictureTestCase(APITestCase):
+    @classmethod
+    def setUp(cls):
+        cls.url = 'cv/picture/'
+        cls.token = 'insert_token_here'
+        cls.cv_data = cv_test_data
+        cls.picture = SimpleUploadedFile(name='test_image.jpg',
+                                         content=open('cv_pic.png', 'rb').read(), content_type='image/png')
