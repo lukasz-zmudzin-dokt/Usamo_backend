51df61f10e77996f18c71daf4cbd1870703224f5
Dmitry <koleda.dmitry@gmail.com>
2020-02-16 14:41:35 +0100

    Added tests

--- src/account/tests.py
+++ src/account/tests.py
@@ -1,74 +1,117 @@
-# Create your tests here.
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
+from .account_status import AccountStatus
+from .account_type import AccountType
+from .models import DefaultAccount, EmployerAccount
+import json
+from django.test import SimpleTestCase
+from .validators import validate_nip
+from django.core.exceptions import ValidationError
 
-from account.account_status import AccountStatus
-from account.models import Account
+
+class NIPTestCase(SimpleTestCase):
+
+    def test_nip_validation_success(self):
+        test_nip = '1234563218'
+        try:
+            validate_nip(test_nip)
+        except ValidationError:
+            self.fail('Raised an exception')
+
+    def test_nip_validation_too_short(self):
+        test_nip = '123456321'
+        self.assertRaises(ValidationError, validate_nip, test_nip)
+
+    def test_nip_too_long(self):
+        test_nip = '12345632189'
+        self.assertRaises(ValidationError, validate_nip, test_nip)
+
+    def test_nip_contains_not_numbers(self):
+        test_nip = '12345632hj'
+        self.assertRaises(ValidationError, validate_nip, test_nip)
+
+    def test_nip_check_sum_does_not_match(self):
+        test_nip = '1234563216'
+        self.assertRaises(ValidationError, validate_nip, test_nip)
 
 
 class RegistrationTestCase(APITestCase):
 
+    @staticmethod
+    def read_test_data(filename):
+        file_path = f'account/test_data/{filename}'
+        with open(file_path) as file:
+            data = json.load(file)
+        return data
+
     @classmethod
     def setUp(cls):
         cls.url = '/account/register/'
-        cls.registration_data = {
-            'username': 'testusername',
-            'email': 'test@email.com',
-            'password': 'testpassword',
-            'first_name': 'testname',
-            'last_name': 'testlastname',
-            'phone_number': '+48123456789',
-            'facility_name': 'test facility name',
-            'facility_address': 'testState, testStreet testNum'
-        }
+        cls.employer_url = '/account/register/employer/'
 
-
-    def test_registration_success(self):
-        self.assertEquals(Account.objects.count(), 0)
-        response = self.client.post(self.url, self.registration_data, format='json')
+    def test_registration_default_success(self):
+        registration_data = self.read_test_data('default_success.json')
+        self.assertEquals(DefaultAccount.objects.count(), 0)
+        response = self.client.post(self.url, registration_data, format='json')
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertEquals(Account.objects.count(), 1)
-        self.assertEquals(Account.objects.get().user.username, self.registration_data['username'])
-        self.assertEquals(Account.objects.get().user.first_name, self.registration_data['first_name'])
-        self.assertEquals(Account.objects.get().user.last_name, self.registration_data['last_name'])
-        self.assertEquals(Account.objects.get().user.email, self.registration_data['email'])
-        self.assertEquals(Account.objects.get().phone_number, self.registration_data['phone_number'])
-        self.assertEquals(Account.objects.get().facility_address, self.registration_data['facility_address'])
-        self.assertEquals(Account.objects.get().facility_name, self.registration_data['facility_name'])
-        self.assertEquals(Account.objects.get().status, AccountStatus.WAITING_FOR_VERIFICATION.value)
+        self.assertEquals(DefaultAccount.objects.count(), 1)
+        account = DefaultAccount.objects.get()
+        self.assertEquals(account.user.username, registration_data['username'])
+        self.assertEquals(account.user.first_name, registration_data['first_name'])
+        self.assertEquals(account.user.last_name, registration_data['last_name'])
+        self.assertEquals(account.user.email, registration_data['email'])
+        self.assertEquals(account.phone_number, registration_data['phone_number'])
+        self.assertEquals(account.facility_address, registration_data['facility_address'])
+        self.assertEquals(account.facility_name, registration_data['facility_name'])
+        self.assertEquals(account.user.status, AccountStatus.WAITING_FOR_VERIFICATION.value)
+        self.assertEquals(account.user.type, AccountType.STANDARD.value)
+        self.assertNotEquals(account.user.password, '')
+        self.assertNotEquals(account.user.password, registration_data['password'])
 
-        self.assertNotEquals(Account.objects.get().user.password, '')
-        self.assertNotEquals(Account.objects.get().user.password, self.registration_data['password'])
+    def test_registration_default_invalid_phone(self):
+        test_data = self.read_test_data('default_invalid_phone.json')
+        response = self.client.post(self.url, test_data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(response.content, b'{"phone_number":"Phone number is invalid"}')
+        self.assertEquals(DefaultAccount.objects.count(), 0)
 
-
-
-    def test_registration_username_exists(self):
-        self.assertEquals(Account.objects.count(), 0)
-        User.objects.create_user(self.registration_data['username'], '', '')
-        self.assertEquals(Account.objects.count(), 1)
-
-        response = self.client.post(self.url, self.registration_data, format='json')
+    def test_registration_default_missing_field(self):
+        test_data = self.read_test_data('default_missing_fields.json')
+        response = self.client.post(self.url, test_data, format='json')
         self.assertEquals(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-        self.assertEquals(response.content, b'{"username":["A user with that username already exists."]}')
+        self.assertEquals(DefaultAccount.objects.count(), 0)
 
+    def test_registration_employer_success(self):
+        registration_data = self.read_test_data('employer_success.json')
+        self.assertEquals(EmployerAccount.objects.count(), 0)
+        response = self.client.post(self.employer_url, registration_data, format='json')
 
-    def test_registration_missing_field(self):
-        for key in self.registration_data.keys():
-            reg_data_with_missing_field = {k: v for (k, v) in self.registration_data.items() if k != key}
-            response = self.client.post(self.url, reg_data_with_missing_field, format='json')
-            self.assertContains(response, text=f'{key}', status_code=status.HTTP_406_NOT_ACCEPTABLE)
-            self.assertContains(response, text='is required', status_code=status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
+        self.assertEquals(EmployerAccount.objects.count(), 1)
+        account = EmployerAccount.objects.get()
+        self.assertEquals(account.user.username, registration_data['username'])
+        self.assertEquals(account.user.first_name, registration_data['first_name'])
+        self.assertEquals(account.user.last_name, registration_data['last_name'])
+        self.assertEquals(account.user.email, registration_data['email'])
+        self.assertEquals(account.phone_number, registration_data['phone_number'])
+        self.assertEquals(account.company_name, registration_data['company_name'])
+        self.assertEquals(account.company_address, registration_data['company_address'])
+        self.assertEquals(account.nip, registration_data['nip'])
+        self.assertEquals(account.user.status, AccountStatus.WAITING_FOR_VERIFICATION.value)
+        self.assertEquals(account.user.type, AccountType.EMPLOYER.value)
+        self.assertNotEquals(account.user.password, '')
+        self.assertNotEquals(account.user.password, registration_data['password'])
 
-    def test_registration_invalid_phone_number(self):
-        invalid_phone_numbers = ['fdgdf', '+48qwertyghjk', '+481234567890', '+48h123456789', '123456789',
-                                 '456', '1345678']
-        for pn in invalid_phone_numbers:
-            self.registration_data['phone_number'] = pn
-            response = self.client.post(self.url, self.registration_data, format='json')
-            self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
-            self.assertEquals(response.content, b'{"phone_number":"Phone number is invalid"}')
+    def test_registration_employer_invalid_nip(self):
+        test_data = self.read_test_data('employer_invalid_nip.json')
+        response = self.client.post(self.employer_url, test_data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(EmployerAccount.objects.count(), 0)
 
-
+    def test_registration_employer_missing_fields(self):
+        test_data = self.read_test_data('employer_missing_fields.json')
+        response = self.client.post(self.employer_url, test_data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertEquals(EmployerAccount.objects.count(), 0)
