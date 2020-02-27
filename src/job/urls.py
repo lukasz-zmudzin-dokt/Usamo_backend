b539aa463f7820e03f8431e8d3527c54a92a98f6
BanyMaciej <banymaciej@gmail.com>
2020-02-27 22:58:13 +0100

    Job offers endpoints - refactoring

--- src/job/urls.py
+++ src/job/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('job-offer/<int:offer_id>', views.JobOfferView.as_view()),
     path('job-offer/', views.JobOfferCreateView.as_view()),
     path('job-offers/', views.JobOfferListView.as_view()),
-    path('offer-interested/<int:offer_id>', views.JobOfferInterestedUsersView.as_view()),
-    path('employer-job-offers/', views.EmployerJobOffersView.as_view()),
+    path('employer/offer-interested/<int:offer_id>', views.JobOfferInterestedUsersView.as_view()),
+    path('employer/job-offers/', views.EmployerJobOffersView.as_view()),
 ]
