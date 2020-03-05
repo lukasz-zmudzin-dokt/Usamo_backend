a60950702e50c9a8fb7ceb6003d40a47c8e1ef75
JanKodowanie <48965206+JanKodowanie@users.noreply.github.com>
2020-03-05 18:51:02 +0100

    custom login view; returns user type

--- src/account/views.py
+++ src/account/views.py
@@ -5,6 +5,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework import views
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import permission_classes, api_view, renderer_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.renderers import JSONRenderer
@@ -141,6 +142,18 @@ class LogoutView(views.APIView):
 
         return Response({'success': 'Successfully deleted the old token'}, status.HTTP_200_OK)
 
+class LoginView(ObtainAuthToken):
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'type': dict(ACCOUNT_TYPE_CHOICES)[user.type]
+        })
 
 class DataView(views.APIView):
 
