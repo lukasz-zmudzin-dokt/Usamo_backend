f1ab4900edd8ea3d5a33b2d6150ae5429d8ed8d1
Dmitry <koleda.dmitry@gmail.com>
2020-04-22 22:02:51 +0200

    Added a postal code validator

--- src/account/validators.py
+++ src/account/validators.py
@@ -15,3 +15,9 @@ def validate_nip(nip: str):
     checksum %= 11
     if checksum != int(nip[9]):
         raise ValidationError('Invalid NIP number')
+
+
+def validate_postal_code(postal_code: str):
+    postal_code_regex = re.compile("[0-9]{2}-[0-9]{3}")
+    if postal_code_regex.match(postal_code) is None:
+        raise ValidationError('Invalid postal code')
