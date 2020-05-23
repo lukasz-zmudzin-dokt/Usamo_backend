c5a592ab8f429cefe46592058cf3f9abd2338bcf
dobrowj1 <01142560@pw.edu.pl>
2020-05-23 22:59:02 +0200

    Working websockets \@/

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.db import models
 from django.conf import settings
 from django.db import models
 from django.db.models import Q
+from account.models import Account
+import uuid
 
 class ThreadManager(models.Manager):
     def by_user(self, user):
@@ -11,8 +12,8 @@ class ThreadManager(models.Manager):
         qs = self.get_queryset().filter(qlookup).exclude(qlookup2).distinct()
         return qs
 
-    def get_or_new(self, user, other_username): # get_or_create
-        username = user.username
+    def get_or_new(self, username, other_username): 
+        user =  Account.objects.get(username=username)
         if username == other_username:
             return None
         qlookup1 = Q(first__username=username) & Q(second__username=other_username)
@@ -23,39 +24,37 @@ class ThreadManager(models.Manager):
         elif qs.count() > 1:
             return qs.order_by('timestamp').first(), False
         else:
-            Klass = user.__class__
-            user2 = Klass.objects.get(username=other_username)
+            user2 = Account.objects.get(username=other_username)
             if user != user2:
-                obj = self.model(
-                        first=user, 
-                        second=user2
-                    )
+                obj = self.model(first=user, second=user2)
                 obj.save()
                 return obj, True
             return None, False
 
 
 class Thread(models.Model):
-    first = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='chat_thread_first')
-    second = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='chat_thread_second')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='chat_thread_first')
+    second = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='chat_thread_second')
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     
     objects = ThreadManager()
 
     @property
-    def room_group_name(self):
+    def room_name(self):
         return f'chat_{self.id}'
 
     def broadcast(self, msg=None):
         if msg is not None:
-            broadcast_msg_to_chat(msg, group_name=self.room_group_name, user='admin')
+            broadcast_msg_to_chat(msg, group_name=self.room_name, user='admin')
             return True
         return False
 
 
 class ChatMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     thread = models.ForeignKey(Thread, null=True, blank=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='sender', on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, verbose_name='sender', on_delete=models.CASCADE)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
