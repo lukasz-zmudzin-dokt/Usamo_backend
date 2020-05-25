5a4dabbb57a76e1e5ea06909eb6123b869d3418d
michauga <mjgajda@icloud.com>
2020-05-26 01:48:02 +0200

    aawd

--- src/tiles/models.py
+++ src/tiles/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-class Tile(models.Model):
-    title = models.CharField(max_length=100)
-    photo = models.URLField(null=True)
-    destination = models.CharField(max_length=100)
-    color = models.CharField(max_length=30)
+class PhotoLayer(models.Model):
     photo_layer_left = models.BooleanField(default=False)
     photo_layer_top = models.BooleanField(default=False)
     photo_layer_right = models.BooleanField(default=False)
+
+
+class Tile(models.Model):
+    title = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='tiles/%Y/%m/%d', null=True)
+    destination = models.CharField(max_length=100)
+    color = models.CharField(max_length=30)
+    photo_layer = models.OneToOneField(PhotoLayer, on_delete=models.CASCADE)
--- src/tiles/serializers.py
+++ src/tiles/serializers.py
@@ -2,9 +2,16 @@ from .models import *
 from rest_framework import serializers
 
 
+class PhotoLayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoLayer
+        fields = '__all__'
+
+
 class TileSerializer(serializers.ModelSerializer):
-    photo = serializers.URLField(required=False)
+    photo = serializers.ImageField(required=False)
+    photo_layer = PhotoLayerSerializer()
 
     class Meta:
         model = Tile
-        fields = '__all__'
+        fields = ['id', 'title', 'destination', 'color', 'photo_layer', 'photo']
