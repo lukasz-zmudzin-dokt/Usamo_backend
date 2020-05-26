a066bb3508acda9dcb17e05668f0e2edec0ce7a1
dobrowj1 <01142560@pw.edu.pl>
2020-05-26 11:05:34 +0200

    Added steps fixture to release tasks

--- src/release-tasks.sh
+++ src/release-tasks.sh
@@ -5,3 +5,4 @@ python manage.py loaddata test_cv.json
 python manage.py loaddata test_blog.json
 python manage.py loaddata test_job.json
 python manage.py loaddata test_videos.json
+python manage.py loaddata test_steps.json
