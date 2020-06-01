50233d33740ca8f1aa1a722cb3b26640337c07ba
dobrowj1 <01142560@pw.edu.pl>
2020-06-02 00:28:56 +0200

    Changed static and media roots

--- src/usamo/settings/prod/prod_settings.py
+++ src/usamo/settings/prod/prod_settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/api.usamodzielnieni.pl/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -200,7 +200,7 @@ SWAGGER_SETTINGS = {
     ],
 }
 DJANGO_NOTIFICATIONS_CONFIG = {'USE_JSONFIELD': True}
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/api.usamodzielnieni.pl/media/'
 MEDIA_URL = '/media/'
 
 # APScheduler
